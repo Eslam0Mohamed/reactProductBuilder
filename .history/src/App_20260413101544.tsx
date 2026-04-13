172784d3664bbcaa3295d9c0719535dfbee6cd11
Eslam Mohamed <eslama202002@gmail.com>
2026-04-13 10:17:46 +0200

    install tailwindCss

--- .history/src/App_20260413101544.tsx
+++ .history/src/App_20260413101544.tsx
@@ -9,7 +9,7 @@ function App() {
 
   return (
     <>
-      <section id="center" className='bg-red-800'>
+      <section id="center" className=''>
         <div className="hero">
           <img src={heroImg} className="base" width="170" height="179" alt="" />
           <img src={reactLogo} className="framework" alt="React logo" />
